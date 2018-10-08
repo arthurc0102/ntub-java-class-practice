4da37022833101a64bbcf1fa9b5cbf98c670c7d8
Arthur Chang <arthurc0102@gmail.com>
2018-10-08 15:56:06 +0800

    Create project and class_1008.practice01

---
+++ src/me/arthurc/class_1008/Practice01.java
@@ -0,0 +1,12 @@
+package me.arthurc.class_1008;
+
+import java.util.Date;
+
+public class Practice01 {
+
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.printf("現在是%1$tp，今天是%1$tA %1$tm/%1$td %1$tY 是今年以來的第 %1$tj 天\n", now);
+    }
+
+}
