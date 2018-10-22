92ffa8b727b53fc23f76d4fa1c8b6766727ce62d
Arthur Chang <arthurc0102@gmail.com>
2018-10-22 15:01:29 +0800

    Add class_1022.Example01

---
+++ src/me/arthurc/class_1022/Example01.java
@@ -0,0 +1,22 @@
+package me.arthurc.class_1022;
+
+public class Example01 {
+
+    public static void main(String[] args) {
+        int num = 0;
+        int[][] targets = {{1, 0, 0, 1, 0}, {1, 0, 1, 1, 0}, {1, 1, 1, 1, 0}};
+
+        for (int[] target: targets) {
+            for (int item: target) {
+                if (item == 0) {
+                    break;
+                }
+
+                num++;
+            }
+        }
+
+        System.out.printf("遇到 0 之前總共有 %d 個 1\n", num);
+    }
+
+}
