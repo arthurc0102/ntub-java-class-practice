77148a7b5aea24612fa937854eb7404413d62af3
Arthur Chang <arthurc0102@gmail.com>
2018-10-22 15:18:11 +0800

    Add class_1022.Example02

---
+++ src/me/arthurc/class_1022/Example02.java
@@ -0,0 +1,22 @@
+package me.arthurc.class_1022;
+
+import java.util.stream.IntStream;
+
+public class Example02 {
+
+    public static void main(String[] args) {
+        int[] intArray = {56, 45, 68, 32};
+
+        // Case1
+        int total = 0;
+        for (int item: intArray) {
+            total += item;
+        }
+
+        System.out.println(total);
+
+        // Case2
+        System.out.println(IntStream.of(intArray).sum());
+    }
+
+}
