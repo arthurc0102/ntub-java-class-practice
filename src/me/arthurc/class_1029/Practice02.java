f18e2ddb1941371e0969a7ef8ada69e7015b607c
Arthur Chang <arthurc0102@gmail.com>
2018-11-20 11:58:26 +0800

    Add class_1029.Practice02

---
+++ src/me/arthurc/class_1029/Practice02.java
@@ -0,0 +1,21 @@
+package me.arthurc.class_1029;
+
+public class Practice02 {
+
+    public static void main(String[] args) {
+        int result = factorial(3);
+        System.out.println(result);
+    }
+
+    private static int factorial(int n) {
+        if (n < 0) {
+            throw new java.lang.Error("Number should bigger than 0");
+        }
+
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+
+        return n * factorial(n-1);
+    }
+}
